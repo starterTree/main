0c216e45c09dc9a55b8a4aeb24d4e07406522c35
Thomas Roche <thomas.roche@inrae.fr>
2022-10-05 21:55:32 +0200

    init plugins

---
+++ plugins/main.py
@@ -0,0 +1,22 @@
+# PLUGINS comment to disable
+
+
+import os, sys
+
+parent = os.path.abspath('.')
+sys.path.insert(1, parent)
+
+
+#import plugins.cmd.main
+#import plugins.enc.main
+#import plugins.k8s.main
+#import plugins.search_mod.main
+#import plugins.ssh.main
+#import plugins.theme.main
+#import plugins.www.main
+#import plugins.manage.main
+#import plugins.config.main
+import dir.main
+#import plugins.readDict.main
+#import plugins.external_config.main
+import debug.main
